c6123abafb43cfd3ad2bdb0e55736e09181c7adb
Berliantama <134850681+Berliantama@users.noreply.github.com>
2025-01-17 21:25:33 +0800

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+// Data koneksi database
+$servername = "localhost";  
+$username = "root";       
+$password = "";             
+$dbname = "belochi";    
+
+// Membuat koneksi
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Cek koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+?>
